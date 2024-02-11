fed657b796eb935564bd3f79520d39daef91a488
Aman-web-dev <amanzhx1234+github@gmail.com>
2024-02-11 10:41:02 +0530

    pack

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
-console.log("hello")
+var express = require('express');
+var cors = require('cors');
+const fetch =(...args)=>
+import (node-fetch).then(({default:fetch})=>fetch(...args));
+var bodyParser= require('body-parser');
+
+
+
+const client_id='d8c29a8e39a37033822a'
+const client_secret="fb427bdd9e6c3c64fea299db8fe9aa9bce2496ef"
+
+
+
+
+var app=express()
+
+app.use(cors())
+
+app.use(bodyParser.json())
+
+
+app.listen(4000,function(){
+    console.log("Cors server Running on Port 4000")
+})
