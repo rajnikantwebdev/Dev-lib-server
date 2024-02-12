c82ca922b2a9ce254e921c1418e4e3265dccbc0d
Rajnikantbhay <rsusishere@gmail.com>
2024-02-12 12:24:44 +0530

    initialze server

--- server.js
+++ server.js
@@ -1,24 +1,22 @@
-var express = require('express');
-var cors = require('cors');
-const fetch =(...args)=>
-import (node-fetch).then(({default:fetch})=>fetch(...args));
-var bodyParser= require('body-parser');
+var express = require("express");
+var cors = require("cors");
+const fetch = (...args) =>
+  import(node - fetch).then(({ default: fetch }) => fetch(...args));
+var bodyParser = require("body-parser");
 
+const client_id = "d8c29a8e39a37033822a";
+const client_secret = "fb427bdd9e6c3c64fea299db8fe9aa9bce2496ef";
 
+var app = express();
 
-const client_id='d8c29a8e39a37033822a'
-const client_secret="fb427bdd9e6c3c64fea299db8fe9aa9bce2496ef"
+app.use(cors());
 
+app.use(bodyParser.json());
 
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
 
-
-var app=express()
-
-app.use(cors())
-
-app.use(bodyParser.json())
-
-
-app.listen(4000,function(){
-    console.log("Cors server Running on Port 4000")
-})
+app.listen(4000, function () {
+  console.log("Cors server Running on Port 4000");
+});
