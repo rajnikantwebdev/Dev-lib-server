cfebfab4d8bc3228337ea4873088c235fe0b70bf
Rajnikantbhay <rsusishere@gmail.com>
2024-02-12 12:58:41 +0530

    using es6 module initialized firebase and nodemon

--- server.js
+++ server.js
@@ -1,16 +1,26 @@
-var express = require("express");
-var cors = require("cors");
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+const app = express();
 const fetch = (...args) =>
   import(node - fetch).then(({ default: fetch }) => fetch(...args));
-var bodyParser = require("body-parser");
 
-const client_id = "d8c29a8e39a37033822a";
-const client_secret = "fb427bdd9e6c3c64fea299db8fe9aa9bce2496ef";
-
-var app = express();
+const firebaseConfig = {
+  apiKey: "AIzaSyDBt60YVWPEvQGMvOTCfyJAuJY0_hU4XRA",
+  authDomain: "devlib-c6572.firebaseapp.com",
+  databaseURL:
+    "https://devlib-c6572-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "devlib-c6572",
+  storageBucket: "devlib-c6572.appspot.com",
+  messagingSenderId: "553953347527",
+  appId: "1:553953347527:web:2ebde7a35ff5917e2cbbe2",
+  measurementId: "G-CM76FGV328",
+};
 
 app.use(cors());
-
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
