d3b5eaf863d5a2647ba016792f282d3506141126
Aman-web-dev <amanzhx1234+github@gmail.com>
2024-02-21 17:40:15 +0530

    CHote Changes

--- server.js
+++ server.js
@@ -12,11 +12,37 @@ const client_secret="fb427bdd9e6c3c64fea299db8fe9aa9bce2496ef"
 
 
 
+
+
 var app=express()
-
 app.use(cors())
-
 app.use(bodyParser.json())
+
+
+app.get('/getAccessToken',async function(req,res){
+
+
+    req.query.code;
+
+    const param ="?client_id="+client_id+"&client_secret="+client_secret+"&code="+req.query.code;
+
+    await fetch('https://github.com/login/oauth/access_token'+param,{
+        method:'POST',
+        headers:{
+           "Accept":'application/json' 
+        }
+    }).then((response)=>{
+        return response.json()
+    }).then((data)=>{
+        console.log(data)
+         res.json(data)
+    })
+
+})
+
+
+
+app.get('/getuserdata')
 
 
 app.listen(4000,function(){
