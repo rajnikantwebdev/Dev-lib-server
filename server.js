4dc221fa8f4eba530539666d50e6c4594867152e
Rajnikantbhay <rsusishere@gmail.com>
2024-04-15 21:27:13 +0530

    minor changes

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import express, { response } from "express";
+import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 import { getAllVideoData, writeUserData } from "./router.js";
