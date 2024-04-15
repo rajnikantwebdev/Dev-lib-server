af6c0b994e009e4a6c1e10c8f56700ed1a90ec6b
Rajnikantbhay <rsusishere@gmail.com>
2024-04-15 20:33:08 +0530

    fixing ACAO error

--- server.js
+++ server.js
@@ -56,11 +56,21 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(function (req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
   res.setHeader(
     "Access-Control-Allow-Headers",
-    "Content-Type, Access-Control-Allow-Headers"
+    "Content-Type, Access-Control-Allow-Headers, Authorization"
   );
+
+  // Handle preflight requests
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
+
   next();
 });
 
