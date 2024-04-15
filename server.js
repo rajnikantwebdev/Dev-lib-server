2753529e49d7cce461f4bf54e26c262a648e58fb
Rajnikantbhay <rsusishere@gmail.com>
2024-04-15 16:31:59 +0530

    access control allow origin changed

--- server.js
+++ server.js
@@ -53,8 +53,9 @@ app.use(
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
   res.setHeader(
     "Access-Control-Allow-Headers",
