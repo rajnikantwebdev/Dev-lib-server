48911865bea0f66ded264d9a3963781a58ee5444
Rajnikantbhay <rsusishere@gmail.com>
2024-04-15 21:11:51 +0530

    fixing CORS errors

--- server.js
+++ server.js
@@ -188,6 +188,7 @@ app.post("/add-yt-vid", (req, res) => {
 
 // get all the youtube video from the database
 app.get("/get-yt-vid", (req, res) => {
+  console.log("Origin header: ", req.headers.origin);
   getAllVideoData()
     .then((response) => {
       res.status(200).send(response.data);
